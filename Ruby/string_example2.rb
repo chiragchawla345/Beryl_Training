719ca4a00fd02ede694d2561202ff4d19e2cdf4b
chiragchawla345 <chiragchawla345@gmail.com>
2019-05-31 12:26:33 +0530

    Second File of String Examples

---
+++ Ruby/string_example2.rb
@@ -0,0 +1,59 @@
+a="hello world"
+puts a.count("er")  #2
+puts a.count(" ")   #1
+puts a.count("erl","hol")  #0 find intersect string in arguments then count
+puts a.count("ho")  #3
+puts a.count("ello","oldw")  #5
+puts a.count("r-w")  #2 characrters between r & w inclusive
+puts a.count("or-w")  #5 Between o & w
+
+#delete
+puts a.delete("or-w") #hell ld
+puts a.delete("a-z","^aeiou")  #delete all consonents
+
+#dump
+puts "hello \n ''".dump   #hello \n ''
+puts "hello \n ''"        #hello
+						   ' '
+
+#each_byte
+"hello".each_byte {|c| print c, ' ' }
+puts ""
+"c \n 100".each_byte {|c| print c, ' ' }
+puts""
+
+#each_line
+"hello\nworld".each_line {|s| print s+" "}  #seperate substring after \n
+"hello\nworld".each_line('l') {|s| print s+" "}  #seperate substring after 'l'
+"hello\bworld".each_line {|s| print s+" "}  #no seperation
+puts "New line"
+"hello\n\n\nworld".each_line('') {|s| print s+" "}   #="hello\n\n\nworld".each_line {|s| print s+" "}
+													 #Seperate after \n\n\n
+
+#hex
+puts "ff".hex
+puts "10".hex  #HexaDecimal to Decimal
+puts "hello".hex #0 in case of error beacuse it is a sequence of character but in hexadecimal only 0-9 and a-f are allowed
+puts "af".hex													 
+
+#include
+puts "hello".include? "eo"  #checks passing string is a substring of calling string or not
+puts "hello world".include? " "
+puts "hello world".include? "ell"
+
+#index
+puts "hello".index(/l./) #2
+puts "hello".index(/e.../) #1
+puts "hello".index(/e..../) #nil
+
+#match
+puts 'hello'.match(/.l./)     #Return matched string
+puts 'hello'=~/.l./ 
+puts "cat o' 9 tails" =~ /9/  #Returns position 
+
+#oct
+puts "123".oct #octal to decimal
+puts "123ckadhs".oct #ignoring last characters
+puts "cdskh123".oct  #0 if fails
+
+"hello\nworld".each_line('ll') {|s| print s+" "}  
